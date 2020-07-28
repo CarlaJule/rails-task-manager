3be386880b6afdadb889323133ac25d12898aae5
Carla Schell <carlaschell@t-online.de>
2020-07-28 12:38:30 +0200

    view completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    set_task
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
 end
