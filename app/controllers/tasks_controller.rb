ca9af0e454b7a1dc459b569877e8749a3d08b5f6
Carla Schell <carlaschell@t-online.de>
2020-07-28 14:59:52 +0200

    all completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,10 +7,40 @@ class TasksController < ApplicationController
     set_task
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to "/tasks"
+  end
+
+  def edit
+    set_task
+  end
+
+  def update
+    set_task
+    @task.update(task_params)
+    redirect_to "/tasks"
+  end
+
+  def destroy
+    set_task
+    @task.destroy
+    redirect_to "/tasks"
+  end
+
   private
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+   params.require(:task).permit(:title, :details, :completed)
   end
 
 end
